5bea76be09d801e150074f423466e3c3bcc8c5b2
Tejas <56431505+tejasvtanna@users.noreply.github.com>
2020-05-28 20:00:32 +0530

    Updates

---
+++ precache-manifest.daeecd4f2120ab9a5ae0c4f3f8f684ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3178a03f65f20dcbd76e8151dbcced9",
+    "url": "/advice-app/index.html"
+  },
+  {
+    "revision": "c2170683a7a3d631334f",
+    "url": "/advice-app/static/css/main.478a95bd.chunk.css"
+  },
+  {
+    "revision": "2122737639fe656de282",
+    "url": "/advice-app/static/js/2.6d89f97c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/advice-app/static/js/2.6d89f97c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2170683a7a3d631334f",
+    "url": "/advice-app/static/js/main.13a74728.chunk.js"
+  },
+  {
+    "revision": "40ea2fc223d118481f3a",
+    "url": "/advice-app/static/js/runtime-main.b99cda3a.js"
+  },
+  {
+    "revision": "91c109fc2e2a14e1e0a445d4314f9334",
+    "url": "/advice-app/static/media/city.91c109fc.jpg"
+  }
+]);
